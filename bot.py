b9d2b3eb4f6f033dd176b84d918c5554650151b8
Thomas Hobohm <superman3275@gmail.com>
2014-04-13 11:45:32 -0500

    Changed name of comment variable to comment_text.

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ def comment(submission, commment):
 	try:
 		submission.add_comment(comment)
 		already_done.append(submission)
+		print "Commented on submission " + vars(submission)["name"]
 	except RateLimitExceeded:
 		queue.put((submission, comment))
 
@@ -31,7 +32,7 @@ while True:
 
 		if not (submission in already_done):
 			author = vars(submission)["author"]
-			comment = ""
+			comment_text = ""
 
 			# Get account age
 			author_created = datetime.datetime.fromtimestamp(int(author.created_utc))
@@ -40,32 +41,32 @@ while True:
 
 			# Check account age
 			if delta.days < 7:
-				comment +=  "WARNING: This poster's account is less than a week old! Trade with caution!\n\n\n"
+				comment_text +=  "WARNING: This poster's account is less than a week old! Trade with caution!\n\n\n"
 			elif delta.days < 31:
-				comment += "WARNING: This poster's account is less than a month old! Trade with caution!\n\n\n"
+				comment_text += "WARNING: This poster's account is less than a month old! Trade with caution!\n\n\n"
 			elif delta.days < 93:
-				comment += "WARNING: This poster's account is less than three months old! Trade with caution!\n\n\n"
+				comment_text += "WARNING: This poster's account is less than three months old! Trade with caution!\n\n\n"
 			else:
-				comment += "This poster's account is older than three months! It fulfills the age requirement!!\n\n\n"
+				comment_text += "This poster's account is older than three months! It fulfills the age requirement!!\n\n\n"
 
 			# Check comment karma
 			if author.comment_karma < 10:
-				comment += "WARNING: This poster has very, very little (less than 10) karma! Trade with caution!\n\n\n"
+				comment_text += "WARNING: This poster has very, very little (less than 10) karma! Trade with caution!\n\n\n"
 			elif author.comment_karma < 100:
-				comment += "WARNING: This poster has little (less than 100) karma! Trade with caution!\n\n\n"
+				comment_text += "WARNING: This poster has little (less than 100) karma! Trade with caution!\n\n\n"
 			elif author.comment_karma < 300:
-				comment += "WARNING: This poster has less-than-average (less than 300) karma! Trade with caution!\n\n\n"
+				comment_text += "WARNING: This poster has less-than-average (less than 300) karma! Trade with caution!\n\n\n"
 			else:
-				comment += "This poster has more than 300 karma! (S)he fulfills the karma requirement!\n\n\n"
+				comment_text += "This poster has more than 300 karma! (S)he fulfills the karma requirement!\n\n\n"
 
 			# Overall
-			count = comment.count("WARNING")
+			count = comment_text.count("WARNING")
 			if count == 2:
-				comment += "OVERALL: Do not trade with this poster! They do not fulfill any of this subreddit's requirements!!"
+				comment_text += "OVERALL: Do not trade with this poster! They do not fulfill any of this subreddit's requirements!!"
 			elif count == 1:
-				comment += "OVERALL: Be careful with this poster! They only fulfill one of this subreddit's requirements!"
+				comment_text += "OVERALL: Be careful with this poster! They only fulfill one of this subreddit's requirements!"
 			elif count == 0:
-				comment += "OVERALL: This poster's account looks great! (Still be careful though)"
+				comment_text += "OVERALL: This poster's account looks great! (Still be careful though)"
 			
-			comment(submission, comment)
+			comment(submission, comment_text)
 	time.sleep(30)
